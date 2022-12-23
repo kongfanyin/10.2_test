a3bfaf3cce49c5e06fa1ce3dffb2f3a969ce48d9
kongfanyin <103018828+kongfanyin@users.noreply.github.com>
2022-12-23 12:10:37 +0800

    10.2

---
+++ 10.2_test/person.h
@@ -0,0 +1,26 @@
+#pragma once
+#include<iostream>
+#include<string>
+using std::string;
+class Person
+{
+public:
+	Person() { lname = "", fname[0] = '\0'; };
+	Person(const string &ln, const char* fn = "Heyyon");
+	void show()const;//first name,last name format
+	void FormalShow();
+	~Person();
+
+private:
+	static const int LIMIT = 25;//静态成员变量
+	string lname;
+	char fname[LIMIT];
+};
+
+Person::Person()
+{
+}
+
+Person::~Person()
+{
+}
